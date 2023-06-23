eba26f42f0941ac56c91fed472ecd1d342571978
CWmain <cameronwhite2478@gmail.com>
2023-06-23 18:33:15 +1000

    Login page base

---
+++ eatery-frontend/src/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
+}
+
+export default LoginPage;
