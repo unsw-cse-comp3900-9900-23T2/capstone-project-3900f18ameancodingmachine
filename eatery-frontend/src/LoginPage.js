225c587d194b78d08396bc49df1bdd4a39c8bf57
CWmain <cameronwhite2478@gmail.com>
2023-06-23 19:41:45 +1000

    Feature: Exteramly basic login page, submission does not currently do anything

--- eatery-frontend/src/LoginPage.js
+++ eatery-frontend/src/LoginPage.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import './LoginPage.css'
 import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
-function LoginPage() {
+
+function LoginForm(){
+
+
+ return (
+    <form className='formSubmission'>
+    <label>
+      Email:
+      <input type="text" name="email"/>
+    </label>
+    <br/>
+    <label>
+      Password:
+      <input type="text" name="email"/>
+    </label>
+    <input type="submit" value="Submit" />
+    </form>
+
+  )
+  
+}
+
+function LoginPage(){  
   return (
     <div>
       <h1>Login Page</h1>
       <Link to="/">Go to Home</Link>
+
+      <LoginForm/>
+   
     </div>
   );
 }
+
+
 
 export default LoginPage;
