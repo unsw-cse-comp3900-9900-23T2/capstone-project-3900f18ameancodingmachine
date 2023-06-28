836e02e3eac3c0e60dc691407bb9d112ca8cf722
CWmain <cameronwhite2478@gmail.com>
2023-06-28 15:23:26 +1000

    Fix: Modified file structure to match

--- eatery-frontend/src/App.js
+++ eatery-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage';
-import LoginPage from './LoginPage';
+import HomePage from './home/HomePage';
+import LoginPage from './user/LoginPage';
 
 function App() {
   return (
