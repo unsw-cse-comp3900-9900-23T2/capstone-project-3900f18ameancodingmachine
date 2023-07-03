ae42fd0aade2a8ac6623b0a6b91433af55d89bc5
allansugi <allansugianto@outlook.com>
2023-07-03 16:54:47 +1000

    bug fix: http request working

--- eatery-frontend/src/home/HomePage.jsx
+++ eatery-frontend/src/home/HomePage.jsx
@@ -8,11 +8,33 @@ import axios from 'axios';
 export default function HomePage() {
   // Null if not logged in, True if Restaurant manager, False if User
   const [isRestaurant, setIsRestaurant] = React.useState(null); // TODO get from backend
-  const results = axios.post('api/user/account', {login: "someone@gmail.com", password: "1500000000000000000000000000"})
-                  .then((res) => {return res})
-                  .catch((err) => console.log("not good"));
-  console.log(results);
- 
+
+  React.useEffect(() => {
+    async function checkCookies() {
+      try {
+        // const login = await axios.post('api/user/login', {login: "restaurantAcc", password: "password"});
+        // get the loginId
+        const result = await axios.get('api/user/');
+        let data = result.data;
+        if (data.success !== 0) {
+          let loginId = data.token.result.id;
+          console.log(loginId)
+          // get EateryAccount, it no result then it will return an error
+          // else it will go to restaurant page
+          const account = await axios.get(`api/user/eatery/${loginId}`)
+          setIsRestaurant(true)
+        }
+      } catch (err) {
+        // if not eatery then it is a user
+        if (err.response.status === 404) {
+          setIsRestaurant(false)
+        }
+      }
+      
+    }
+
+    checkCookies()
+  })
   
   return (
     // Defaults to User Home Page if not logged in
