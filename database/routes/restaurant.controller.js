8b0161fb7e7c0d4a171786376fb9e8a1514373c5
Ahmed Al Abassy <z5312664@ad.unsw.edu.au>
2023-07-26 18:56:43 +1000

    chore: retauraunt documentation

--- database/routes/restaurant.controller.js
+++ database/routes/restaurant.controller.js
@@ -1,13 +1,18 @@
 import { createNewVoucher, getAllCuisines, getAllEateries, updateExistingDescription, createEateryAccount, createRestaurantDietary, getAllEateryVouchers } from './restaurant.service.js'
 
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @returns result called from service function with statuscode 200
+ * @returns object with success: 0 and database connection error with statuscode 500
+ */
 export async function createVoucher (req, res) {
     try {
         const body = req.body
-        console.log(body)
         const result = await createNewVoucher(body)
         return res.status(200).json(result)
     } catch (err) {
-        console.log(err)
         return res.status(500).json({
             success: 0,
             message: 'Database connection error'
@@ -15,13 +20,19 @@ export async function createVoucher (req, res) {
     }
 }
 
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @returns result called from service function with statuscode 200
+ * @returns object with success: 0 and database connection error with statuscode 500
+ */
 export async function updateDescription (req, res) {
     try {
         const body = req.body
         const result = await updateExistingDescription(body)
         return res.status(200).json(result)
     } catch (err) {
-        console.log(err)
         return res.status(500).json({
             success: 0,
             message: 'Database connection error'
@@ -29,13 +40,19 @@ export async function updateDescription (req, res) {
     }
 }
 
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @returns result called from service function with statuscode 200
+ * @returns object with success: 0 and database connection error with statuscode 500
+ */
 export async function getEateryVouchers (req, res) {
     try {
         const id = req.params.id;
         const results = await getAllEateryVouchers(id)
         return res.status(200).json(results)    
     } catch (error) {
-        console.log(error)
         return res.status(500).json({
             success: 0,
             message: 'Database connection error'
@@ -43,10 +60,16 @@ export async function getEateryVouchers (req, res) {
     }
 }
 
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @returns result called from service function with statuscode 200
+ * @returns object with success: 0 and database connection error with statuscode 500
+ */
 export async function getCuisines (req, res) {
     try {
         const results = await getAllCuisines()
-        console.log(results)
         return res.status(200).json(results)
     } catch (error) {
         return res.status(500).json({
@@ -56,10 +79,16 @@ export async function getCuisines (req, res) {
     }
 }
 
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @returns result called from service function with statuscode 200
+ * @returns object with success: 0 and database connection error with statuscode 500
+ */
 export async function getEateries (req, res) {
     try {
         const results = await getAllEateries()
-        console.log(results)
         return res.status(200).json(results)
     } catch (error) {
         return res.status(500).json({
@@ -69,16 +98,23 @@ export async function getEateries (req, res) {
     }
 }
 
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @returns result called from service function with statuscode 200
+ * @returns object with success: 0 and duplicate record message with statuscode 409
+ * @returns object with success: 0 and database connection error with statuscode 500
+ */
 export async function createEatery (req, res) {
     try {
         const body = req.body
         const result = await createEateryAccount(body)
         if (result.success === 0) {
-            return res.status(400).json(result)
+            return res.status(409).json(result)
         }
         return res.status(200).json(result)
     } catch (err) {
-        console.log(err)
         return res.status(500).json({
             success: 0,
             message: 'Database conenction error'
